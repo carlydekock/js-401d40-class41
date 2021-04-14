cb59e7ab4adddf85461ddd7d9772ff6156f9698d
CARLY DEKOCK <CARLY.M.DEKOCK@GMAIL.COM>
2021-04-13 21:41:24 -0700

    finished lab requirements and updated README

--- App.js
+++ App.js
@@ -37,6 +37,7 @@ export default function App() {
           </TouchableOpacity>
         </View>
       </Camera>
+
     </View>
   );
 }
@@ -61,7 +62,7 @@ const styles = StyleSheet.create({
   },
   camera: {
     width: '75%',
-    height: '75%',
+    height: '60%',
   },
   button: {
     height: 40,
@@ -72,60 +73,4 @@ const styles = StyleSheet.create({
   }
 
 });
-// import React, { useState, useEffect } from 'react';
-// import { StyleSheet, Text, View, Button, FlatList, Linking } from 'react-native';
 
-// // expo requires that we install these via the command line
-// import * as Permissions from 'expo-permissions';
-// import * as Contacts from 'expo-contacts';
-
-// export default function App() {
-
-//   const [contacts, setContacts] = useState([]);
-//   const [permissions, setPermissions] = useState(false);
-
-//   const getPermissions = async () => {
-//     const { status } = await Permissions.askAsync(Permissions.CONTACTS);
-//     setPermissions(true);
-//   }
-
-//   const showContacts = async () => {
-//     const contactList = await Contacts.getContactsAsync();
-//     console.log(contactList);
-//     setContacts(contactList.data);
-//   }
-
-//   const call = (contact) => {
-//     let phoneNumber = contact.phoneNumbers[0].number.replace(/[\(\)\-\s+]/g, '');
-//     let link = `tel:${phoneNumber}`;
-//     Linking.canOpenURL(link)
-//       .then(() => Linking.openURL(link));
-//   }
-
-//   useEffect(() => {
-//     // if (permissions) {
-//     //   showContacts();
-//     // } else {
-//     getPermissions();
-//     // }
-//   }, []);
-
-//   return (
-//     <View style={styles.container}>
-//       <Button
-//         onPress={showContacts}
-//         title="Press to see contacts"
-//       />
-//       <View>
-//         <Text>Contacts</Text>
-//         <FlatList
-//           data={contacts}
-//           keyExtractor={item => item.id}
-//           renderItem={({ item }) => <Button title={item.name} onPress={() => call(item)} />}
-//         />
-//       </View>
-//       {/* <Text style={styles.text}>New Stuff!</Text>
-//       <Text style={styles.largerText}>More text</Text> */}
-//     </View>
-//   );
-// }
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# js-401d40-class41
+# Lab 41: React Native Camera App
+
+Camera app that allows you to flip the view forwards and backwards.
+
+## How To Use
+
+- Open the app.
+- Allow to access your camera.
+- Use the top button "Flip Camera" to toggle between forwards and backwards views for the camera.
+
+## Credits and Collaborators
+
+- Jason Quaglia
+- Jason Dormier
+- Nick Magruder
+
+## Resources
+
+- Troubleshooting setup help from Jacob
+- Expo [docs](https://docs.expo.io/versions/v40.0.0/sdk/camera/)
